e91db8b995bb61da27606ce8c2f5d2802ad9b052
Saya <sajkasajko@gmail.com>
2023-02-22 12:37:38 +0600

    .

--- tsis4/Python date/3.py
+++ tsis4/Python date/3.py
@@ -1,4 +1,5 @@
 import datetime
 print(datetime.datetime.now())
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+print(datetime.datetime.now().replace(microsecond=0))
 print(datetime.datetime.now().replace(microsecond=0).isoformat())
