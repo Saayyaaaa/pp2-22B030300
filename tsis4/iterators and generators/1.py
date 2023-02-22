ce07724b157affcb861c30dc5a86b12533692259
Saya <sajkasajko@gmail.com>
2023-02-22 12:03:44 +0600

    .

--- tsis4/iterators and generators/1.py
+++ tsis4/iterators and generators/1.py
@@ -1,11 +1,11 @@
 def square_generator(N):
-    for i in range(1, N + 1, +1):
+    for i in range(1, N + 1, +1):  #N = 5,    1, 2, 3, 4, 5
         yield i * i
 
 for square in square_generator(int(input())):
     print(square)
 
 N = int(input())
-a = (i**2 for i in range(1, N+1, +1))
+a = (i**2 for i in range(0, N+1, +1))
 for i in a:
     print(i)
