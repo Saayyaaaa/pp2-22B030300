dd161c7cf3db7f897b7ad8261b6655a157adc7a4
Saya <sajkasajko@gmail.com>
2023-02-22 12:37:58 +0600

    test

--- tsis4/Math library/3.py
+++ tsis4/Math library/3.py
@@ -1,10 +1,10 @@
 import math
 
 num_sides = int(input())
-side_length = float(input())
+side_length = int(input())
 
 def polygon_area(n_sides, length):
     area = (n_sides * (length ** 2)) / (4 * math.tan((math.pi) / n_sides))
-    print(int(float(area)))
+    print(int(area))
 
 polygon_area(num_sides, side_length)
