881d898d3ea0e0247c0f6e54fd2d183f9264b353
Saya <sajkasajko@gmail.com>
2023-03-01 15:41:21 +0600

    .

--- tsis5/7.py
+++ tsis5/7.py
@@ -2,6 +2,6 @@ def snake_to_camel(snake_str):
     components = snake_str.split('_')
     return components[0] + ''.join(x.title() for x in components[1:])
 
-my_string = "my_snake_case_string"
+my_string = input()
 new_string = snake_to_camel(my_string)
 print(new_string)
