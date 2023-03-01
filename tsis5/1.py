f707dc73f744a3d096f00b311f843d273a302313
Saya <sajkasajko@gmail.com>
2023-03-01 13:45:23 +0600

    .

--- tsis5/1.py
+++ tsis5/1.py
@@ -1,11 +1,11 @@
 import re
 
-pattern = r'a[b]*'
+pattern = r'^a[b]*$'
 
-test_strings = ['ab', 'abb', 'a', 'acb', 'abbb', 'abbbb', 'b']
+test_strings = ['ab', 'abb', 'a', 'acb', 'abbgb', 'abbbb', 'b']
 
 for s in test_strings:
     if re.match(pattern, s):
-        print(s, "matches the pattern.")
+        print(f"{s}: True")
     else:
-        print(s, "does not match the pattern.")
+        print(f"{s}: False")
--- tsis5/2.py
+++ tsis5/2.py
@@ -2,7 +2,7 @@ import re
 
 pattern = r'a[b]{2,3}'
 
-test_strings = ['ab', 'abb', 'a', 'acb', 'abbb', 'abbbb', 'b']
+test_strings = ['ab', 'abb', 'a', 'acb', 'abbbs', 'abbbb', 'b']
 
 for s in test_strings:
     if re.match(pattern, s):
