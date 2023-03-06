7901a50bc43fdfcb653ace2abcd8c2f4cac6545a
Saya <sajkasajko@gmail.com>
2023-03-06 18:30:35 +0600

    me

---
+++ me/28.py
@@ -0,0 +1,7 @@
+a = "Hello"
+b = ("Hello")
+c = ["Hello"]
+
+print(a.__sizeof__())
+print(b.__sizeof__())
+print(c.__sizeof__())
---
+++ me/29.py
@@ -0,0 +1,5 @@
+# import math
+a = {'a', 'c', 'a', 'g', 'h', 'g', 'f'}
+# print(a)
+print(set(a))
+print(sorted(list(a)))
