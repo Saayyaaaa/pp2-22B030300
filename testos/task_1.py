c365f1959dd1f82d8910a89f83b4f7ffb91b15e9
Saya <sajkasajko@gmail.com>
2023-03-07 14:38:12 +0600

    new

--- testos/task_1.py
+++ testos/task_1.py
@@ -1,3 +1,7 @@
 import os
-
-(os.remove('"C:\Users\Сая\Desktop\pp2-22B030300\testos\task_1.txt"'))
+f = open('task_1.txt', 'r+')
+print(f.read())
+f.write('\nhello kbtu - 1\n')
+f.write('1 2 3')
+print(f.read())
+f.close()
