1d1dc1e2b938cfd730c8864d65a99ff2ab636627
Saya <sajkasajko@gmail.com>
2023-05-02 13:13:28 +0600

    .

--- tsis10/phonebook.py
+++ tsis10/phonebook.py
@@ -1,83 +1,154 @@
-import psycopg2
 from config import config
+import psycopg2
 import csv
-
-try:
-    parameters = config() #connection
-    connection = psycopg2.connect(**parameters)
-
-    connection.autocommit = True
+import os
 
 
-    #create new table
-    '''with connection.cursor() as cursor:
-        cursor.execute(
-            """CREATE TABLE users(
-            id serial PRIMARY KEY,
-            first_name VARCHAR(50) NOT NULL,
-            phone_number NUMERIC(11) NOT NULL);"""
-        )
-        print("[INFO] Table was succesfully created")
-    '''
+parameters = config()
+conn = psycopg2.connect(**parameters)
+cursor  = conn.cursor()
+
+
+def add_data():
+    rows = []
+    mode = int(input("Select input:\n1 - read from console input\nEnter: "))
+
+    if mode not in [1, 2]:
+        print("Error")
+        return
     
-    #insert data into table
-    '''with connection.cursor() as cursor:
-        cursor.execute(
-            """INSERT INTO users (first_name, phone_number) VALUES ('John', '87777777777');"""
-        )
-        print("[INFO] Data succesfully inserted")
-    '''
-
-    #import data form csv 
-    with open('sample.csv', 'r') as f:
-        reader = csv.reader(f)
-        next(reader)  # Skip the header row
-
-        # Iterate over the rows in the CSV file and insert them into the database
-        with connection.cursor() as cursor:
-            for row in reader:
-                cursor.execute(
-                    "INSERT INTO users (first_name, phone_number) VALUES (%s, %s)",
-                    (row[0], row[1])
-                )
-            print("[INFO] Data from csv file succesfully inserted")
+    if mode == 1:
+        name   = input("Enter name: ")
+        number = input("Enter number: ")
+        
+        if "--" in name or "--" in number:
+            print("Error")
+            return
+        
+        rows.append([name, number])
     
-    #updating information
-    '''with connection.cursor() as cursor:
-        cursor.execute(
-            """UPDATE users SET first_name = 'Jonathan' WHERE id = 1;"""
-        )
-        print("[INFO] Data succesfully updated")
-    '''
+    else:
+        path = input("Enter absolute or relative path: ")
+        
+        if not os.path.exists(path):
+            print("Path does not exist")
+            return
+        
+        with open(path) as file:
+            data = csv.reader(file)
+            next(data)  # skipping header
 
-    #get data from table 
-    '''with connection.cursor() as cursor:
-        cursor.execute(
-            """SELECT first_name, phone_number FROM users WHERE id = 2;"""
-        )
-        print(cursor.fetchone())
-    '''
+            for row in data:
+                if "--" in row[0] or "--" in row[1]: # preventing SQL-injection
+                    print("Error")
+                    return
+                else:
+                    rows.append(row)
+    
+    for row in rows:
+        cursor.execute(f"INSERT INTO PhoneBook (name, number) VALUES ('{row[0]}', '{row[1]}')")
+
+    conn.commit()
+    print("Done")
+
+def update_data():
+    initial_name   = input("Enter name of contact: ")
+    initial_number = input("Enter number of contact: ")
+    if "--" in initial_name or "--" in initial_number:
+        print("Error")
+        return
+
+    cursor.execute(f"SELECT * FROM PhoneBook WHERE name = '{initial_name}' AND number = '{initial_number}'")
+    if cursor.rowcount == 0:
+        print("Contact does not exist")
+        return
+
+    mode = input("Update name or number:\nname - update name\nnumber - update number\nEnter: ")
+    if mode not in ["name", "number"]:
+        print("Error")
+        return
+    
+    new_info = input(f"Enter new {mode}: ")
+    if "--" in new_info:
+        print("Error")
+        return
+    
+    cursor.execute(f"UPDATE PhoneBook SET {mode} = '{new_info}' WHERE name = '{initial_name}' AND number = '{initial_number}'")
+    
+    conn.commit()
+    print("Done")
+
+
+
+
+def query_data():
+    query_mode = int(input(
+        "Choose query mode:\n1 - search by name\n2 - search by number\n3 - find starts with\n4 - search any attribute\nEnter: "
+        ))
+    
+    if query_mode not in [1, 2, 3, 4]:
+        print("Error")
+        return
+
+    query_value = input("Enter query: ")
+    if "--" in query_value:
+        print("Error")
+        return
+    
+    query_dict = {
+        1 : f"name = '{query_value}'",
+        2 : f"number = '{query_value}'",
+        3 : f"starts_with(name, '{query_value}') OR starts_with(number, '{query_value}')",
+        4 : f"name ILIKE '%{query_value}%' OR number ILIKE '%{query_value}%'"
+        }
+
+    cursor.execute(f"SELECT * FROM PhoneBook WHERE {query_dict[query_mode]}")
+
+    result = cursor.fetchall()
+    if len(result) == 0:
+        print("No results")
+    else:
+        print("Results:")
+
+    for row in result:
+        print(row)
+
+
+
+
+def delete_data():
+    mode = input("Select delete by:\nname - delete by name\nnumber - delete by number\nEnter: ")
+    if mode not in ["name", "number"]:
+        print("Error")
+        return
+    
+    info = input(f"Enter {mode}: ")
+    if "--" in info:
+        print("Error")
+        return
+
+    cursor.execute(f"DELETE FROM PhoneBook WHERE {mode} = '{info}'")
+    
+    conn.commit()
+    print("Done")
+
+
+
+
+def main():
+    functions = {1 : add_data, 2 : update_data, 3 : query_data, 4 : delete_data}
+    mode = int(input("Select mode:\n1 - add data\n2 - update data\n3 - query data\n4 - delete data\nEnter: "))
+
+    if mode not in [1, 2, 3, 4]:
+        print("Error")
+        return
+    
+    functions[mode]()
+
+    cursor.close()
+    conn.close()
     
 
-    #delete data from table
-    '''with connection.cursor() as cursor:
-        cursor.execute(
-            """DELETE FROM users WHERE id = 1;"""
-        )
-        print("[INFO] Data succesfully deleted")
-    '''
 
-    #delete whole table
-    '''with connection.cursor() as cursor:
-        cursor.execute(
-            """DROP TABLE users"""
-        )
-        print("[INFO] Table was succesfully deleted")
-    '''
-
-except Exception as _ex:
-    print("[INFO] Error while working with PostgreSQL", _ex)
-finally:
-    if connection:
-        connection.close()
-        print("[INFO] PostgreSQL connection closed")
+if __name__ == "__main__":
+    main()
